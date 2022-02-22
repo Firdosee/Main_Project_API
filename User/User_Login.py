4148b1f94419b61c9d1c9fbd9544b2b51437cdba
Saleem Khan <alfotech512@gmail.com>
2022-02-22 15:48:23 +0530

    changed the login code

--- User/User_Login.py
+++ User/User_Login.py
@@ -2,32 +2,24 @@ import json
 import requests
 from Data_Config.Excel import Excel_Data
 from Logger.Base_Logger import abc_test_Base
-
+baseURI = "https://hqm-gateway-urtjok3rza-wl.a.run.app/"
 class Test_User_Login:
     def test_login_user(self):
         try:
-
-            e = Excel_Data.getAPIData()
-
             log = abc_test_Base.getLogger()
             log.info("Logging in as admin")
-
-            base_dict = e.getAPIData("BaseData", "URL")
-
-            # Providing the URL
-            URL = base_dict['BaseURL'] + base_dict['RegistrationURL']
-            log.info("URL is provided")
-
-            headers = e.getAPIData("Header", "Header")
+            url = baseURI + 'user/login/generate-token'
+            headers = {'content-type': 'application/json'}
             dataObj = Excel_Data()
             dictionaryData = dataObj.getAPIData("User", "Register_user_madara")
-
+            userName = str(dictionaryData['userName'])
+            password = str(dictionaryData['password'])
             log.info("Fetching payload data for admin")
             payload = {
-                "userName": str(dictionaryData['userName']),
-                "password": str(dictionaryData['password'])
+                "userName": "" + userName + "",
+                "password": "" + password + ""
             }
-            response = requests.post(URL, data=json.dumps(payload), headers=headers)
+            response = requests.post(url, data=json.dumps(payload), headers=headers)
             statuscode = response.status_code
             data = response.json()
             bearer_token = data['token']
@@ -37,6 +29,5 @@ class Test_User_Login:
             assert statuscode == 200
             log.info("Successfully Logged in")
             return bearer_token
-
         except Exception as e:
             print("Exception occured", e)
